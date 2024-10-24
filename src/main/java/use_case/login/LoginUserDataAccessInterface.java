41810b080e5e3b7e0c3dc678332adec3fa57b65d
BobbyYsq <bobbyysq@gmail.com>
2024-10-24 14:58:13 -0400

    Task 2.1

--- src/main/java/use_case/login/LoginUserDataAccessInterface.java
+++ src/main/java/use_case/login/LoginUserDataAccessInterface.java
@@ -27,4 +27,10 @@ public interface LoginUserDataAccessInterface {
      */
     User get(String username);
 
+    /**
+     * Set current user.
+     * @param name the username to look up
+     */
+    void setCurrentUser(String name);
+
 }
